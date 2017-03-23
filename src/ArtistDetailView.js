dbf38f49d48cdccc53133cfd08a8f2fbc465eb01
Edwin <mardwintrader@gmail.com>
2017-03-23 19:17:49 -0400

    send and store comments to firebase

--- src/ArtistDetailView.js
+++ src/ArtistDetailView.js
@@ -2,19 +2,46 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
+import { firebaseAuth, firebaseDatabase } from './firebase'
 import ArtistBox from './ArtistBox'
 
 export default class ArtistDetailView extends Component {
+  handleSend = () => {
+    const { text } = this.state
+    const artistCommentsRef = this.getArtistCommentsRef()
+    var newCommentRef = artistCommentsRef.push()
+    newCommentRef.set({ text });
+  }
+
+  getArtistCommentsRef = () => {
+    const { id } = this.props.artist
+    return firebaseDatabase.ref(`comments/${id}`)
+  }
+
+  handleChangeText = (text) => this.setState({text})
+
   render() {
     const artist = this.props.artist
 
     return (
       <View style={styles.container}>
         <ArtistBox artist={artist} />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Opina sobre este artista"
+            onChangeText={this.handleChangeText}
+          />
+          <TouchableOpacity onPress={this.handleSend}>
+            <Icon name="ios-send-outline" size={30} color="gray" />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -26,4 +53,19 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgray',
     paddingTop: 70,
   },
+  inputContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    height: 50,
+    backgroundColor: 'white',
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  input: {
+    height: 50,
+    flex: 1
+  }
 });
